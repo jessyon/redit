2311e565e406df62be29bf0c68262ac47fe75a9b
jessyon <kateyon@gmail.com>
2016-01-14 21:46:15 +1000

    soyun's favorite photos to use for web design

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body>
+<h1>hi mr. yang</h1>
+
+</body>
+
+
+</html>
+
+
+
+
+
+
+<!--
+<html>
+<head><link href="style.css" rel="stylesheet" type="text/css"></head>
+<body>
+
+
+<div id="top"><br/><br/><br/><br/><span style="color:#ffffff;"><h1>Welcome to Soyun's World ! </br>
+Soyun's So yum yum World !</h1>
+<h3>(warning) Complicated...    :)</h3></span></div>
+<nav><center>About Me | My Shop | Blogs | Photos | Videos | Contact me</center> </nav>
+<div id="wrapper">
+<article><h1>Welcome !</h1>
+	<section><h2>Discover Yummy <span style="color:#e10072">Soyun</span> !</h2>
+	<p>I am 33 years old and soon I will be 34 ! I am Korean and I am a happy person with great visions for life ! </p></section>
+	<section><h2><span style="color:#e10072">Soyun</span>'s Interests !</h2>
+	<p>Relationships, God,  psychology, Health, Problems solving for life !</p></section>
+	</article>
+<aside><h3>menu</h3><p>: )</p></aside>
+<div id ="footer"></div>
+
+</div>
+</body>
+
+</html> -->
